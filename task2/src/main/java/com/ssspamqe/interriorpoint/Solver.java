ec32686616493f174fe76b82f4f224a51fbe1103
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-10-29 02:18:04 +0300

    Add minimization and maximization to solver

--- task2/src/main/java/com/ssspamqe/interriorpoint/Solver.java
+++ task2/src/main/java/com/ssspamqe/interriorpoint/Solver.java
@@ -1,28 +1,39 @@
 package com.ssspamqe.interriorpoint;
 
+import com.ssspamqe.interriorpoint.exceptions.InvalidCpMatrixException;
 import com.ssspamqe.matrix.Coordinate;
 import com.ssspamqe.matrix.Matrix;
 import com.ssspamqe.matrix.util.MatrixUtils;
 
 public class Solver {
 
-    public double[] solve(double[] initialPoint, Matrix A, Matrix C, double alpha, int iterations) {
+    public double[] maximize(double[] initialPoint, Matrix A, Matrix C, double alpha, int iterations) {
         var solution = MatrixUtils.toColumnVector(initialPoint);
 
         for (int i = 0; i < iterations; i++) {
-            solution = proceedIteration(solution, A, C, alpha);
+            solution = proceedMaximizationIteration(solution, A, C, alpha);
         }
 
         return solution.getColumn(0);
     }
 
-    private Matrix proceedIteration(Matrix solution, Matrix A, Matrix C, double alpha) {
+    public double[] minimize(double[] initialPoint, Matrix A, Matrix C, double alpha, int iterations) {
+        var solution = MatrixUtils.toColumnVector(initialPoint);
+
+        for (int i = 0; i < iterations; i++) {
+            solution = proceedMinimizationIteration(solution, A, C, alpha);
+        }
+
+        return solution.getColumn(0);
+    }
+
+    private Matrix proceedMaximizationIteration(Matrix solution, Matrix A, Matrix C, double alpha) {
         var D = MatrixUtils.toDiagonalMatrix(solution.getColumn(0));
         var newA = A.multiply(D);
         var newC = D.multiply(C);
         var P = calculatePMatrix(newA);
         var cp = P.multiply(newC);
-        var newX = calculateNewX(alpha, cp);
+        var newX = calculateNewXForMaximization(alpha, cp);
         return D.multiply(newX);
 
     }
@@ -35,20 +46,62 @@ public class Solver {
         return idenity.minus(multiplication);
     }
 
-    private Matrix calculateNewX(double alpha, Matrix cp) {
+    private Matrix calculateNewXForMaximization(double alpha, Matrix cp) {
         var columnVectorOfOnes = buildColumnVectorOfOnes(cp.getRows());
-        double v = getV(cp);
+        double v = getVForMaximization(cp);
         var buf = cp.multiply(alpha / v);
         return columnVectorOfOnes.plus(buf);
     }
 
-    private double getV(Matrix cp) {
+    private double getVForMaximization(Matrix cp) {
         int n = cp.getRows();
-        double mx = Math.abs(cp.getAt(Coordinate.of(0, 0)));
+        double mx = 0;
         for (int i = 0; i < n; i++) {
-            double abs = Math.abs(cp.getAt(Coordinate.of(i, 0)));
-            mx = Math.max(mx, abs);
+            var coordinate = Coordinate.of(i, 0);
+            var value = cp.getAt(coordinate);
+            if (value < 0) {
+                mx = Math.max(mx, Math.abs(value));
+            }
         }
+        if (mx == 0) {
+            throw new InvalidCpMatrixException("Matrix is not feasible");
+        }
+        return mx;
+    }
+
+    private Matrix proceedMinimizationIteration(Matrix solution, Matrix A, Matrix C, double alpha) {
+        var D = MatrixUtils.toDiagonalMatrix(solution.getColumn(0));
+        var newA = A.multiply(D);
+        var newC = D.multiply(C);
+        var P = calculatePMatrix(newA);
+        var cp = P.multiply(newC);
+        var newX = calculateNewXForMinimization(alpha, cp);
+        return D.multiply(newX);
+
+    }
+
+    private Matrix calculateNewXForMinimization(double alpha, Matrix cp) {
+        var columnVectorOfOnes = buildColumnVectorOfOnes(cp.getRows());
+        double v = getVForMinimization(cp);
+        var buf = cp.multiply(alpha / v);
+        return columnVectorOfOnes.plus(buf);
+    }
+
+    private double getVForMinimization(Matrix cp) {
+        int n = cp.getRows();
+        double mx = Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            var coordinate = Coordinate.of(i, 0);
+            var value = cp.getAt(coordinate);
+            if (value < 0) {
+                mx = Math.max(mx, Math.abs(value));
+            }
+        }
+
+        if (mx == 0) {
+            throw new InvalidCpMatrixException("Matrix is not feasible");
+        }
+
         return mx;
     }
 
